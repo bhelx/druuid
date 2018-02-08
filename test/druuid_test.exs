f92b9ff2a3fbe46c82525027a9d840123c2a704a
Michael Bearne <michael@bearne.com>
2018-02-08 10:08:59 +0000

    compiling in elixir 1.6 and removed warnings

--- test/druuid_test.exs
+++ test/druuid_test.exs
@@ -11,22 +11,22 @@ defmodule DruuidTest do
   test "ids generated at the same time should still be unique" do
     timestamp = :calendar.universal_time |> :calendar.datetime_to_gregorian_seconds
 
-    id = Druuid.gen_from_values(0, :random.uniform(), timestamp)
+    id = Druuid.gen_from_values(0, :rand.uniform(), timestamp)
     assert(id > 0)
-    assert(id != Druuid.gen_from_values(0, :random.uniform(), timestamp))
+    assert(id != Druuid.gen_from_values(0, :rand.uniform(), timestamp))
   end
 
   test "ids should be sortable by time generated" do
     timestamp = :calendar.universal_time |> :calendar.datetime_to_gregorian_seconds
 
-    id = Druuid.gen_from_values(0, :random.uniform(), timestamp)
+    id = Druuid.gen_from_values(0, :rand.uniform(), timestamp)
     assert(id > 0)
-    assert(id < Druuid.gen_from_values(0, :random.uniform(), timestamp + 1))
+    assert(id < Druuid.gen_from_values(0, :rand.uniform(), timestamp + 1))
   end
 
   test "convert to/from base 36 strings" do
     timestamp = :calendar.universal_time |> :calendar.datetime_to_gregorian_seconds
-    id = Druuid.gen_from_values(0, :random.uniform(), timestamp)
+    id = Druuid.gen_from_values(0, :rand.uniform(), timestamp)
 
     output =
       id
